5154a9e80cafa2de5e01ba5ebef7f7450312719c
Sofus Albertsen <sofusalbertsen@users.noreply.github.com>
2025-08-20 09:37:03 +0200

    feat: Add KOSLI_ARTIFACT_FLOW environment variable for getting the artifact trail

--- scripts/get-trail.js
+++ scripts/get-trail.js
@@ -1,6 +1,7 @@
 const { execSync } = require('child_process');
 
 const image = process.env.IMAGE;
+const flow = process.env.KOSLI_ARTIFACT_FLOW;
 if (!image) {
   console.error('IMAGE env variable not set');
   process.exit(1);
@@ -12,7 +13,7 @@ try {
   execSync(`kosli search ${fingerprint} -o json > data.json`, { encoding: 'utf-8' });
   const trail = execSync(`jq -r '.artifacts[0].trail' < data.json`, { encoding: 'utf-8' }).trim();
   console.log(`Trail: ${trail}`);
-  execSync(`kosli get trail -o json ${trail} > trail.json`, { stdio: 'inherit' });
+  execSync(`kosli get trail --flow=${flow} -o json ${trail} > trail.json`, { stdio: 'inherit' });
 } catch (err) {
   console.error(err.message);
   process.exit(1);
